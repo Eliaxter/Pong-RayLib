3a2de09829601d3c4d4fbfc004f90ddefb7b29f9
Elias Brucart <eliasbrucart@hardgames.com.ar>
2019-09-19 18:02:30 -0300

    We added comments before separation

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,105 +1,106 @@
 #include "raylib.h"
+#include <stdlib.h>
 #include <time.h>
 //Screen
-const int screenWidth = 800;
-const int screenHeight = 450;
-const int minScreenW = 0;
-const int minScreenH = 0;
-const int screenBorderX = -2;
-const int halfScreenWidth = 402;
-const int halfScreenHeight = 227;
-const int halfNegativeScreenWidth = 398;
-const int halfNegativeScreenHeight = 223;
-const int randomPowerUPW1 = 30;
-const int randomPowerUPW2 = 770;
-const int randomPowerUPH1 = 30;
-const int randomPowerUPH2 = 420;
+const int screenWidth = 800; //gameplay
+const int screenHeight = 450; //gameplay
+const int minScreenW = 0; //gameplay
+const int minScreenH = 0; //gameplay
+const int screenBorderX = -2; //gameplay
+const int halfScreenWidth = 402; //gameplay
+const int halfScreenHeight = 227; //gameplay
+const int halfNegativeScreenWidth = 398; //gameplay
+const int halfNegativeScreenHeight = 223; //gameplay
+const int randomPowerUPW1 = 30; //gameplay
+const int randomPowerUPW2 = 770; //gameplay
+const int randomPowerUPH1 = 30; //gameplay
+const int randomPowerUPH2 = 420; //gameplay
 //Players
-Rectangle player1;
-Rectangle player2;
+Rectangle player1; //palette
+Rectangle player2; //palette
 //Player1
-const int player1X = 2;
-const int player1Y = 200;
+const int player1X = 2; //palette
+const int player1Y = 200; //palette
 //Player2
-const int player2X = 747;
-const int player2Y = 200;
+const int player2X = 747; //palette
+const int player2Y = 200; //pàlette
 
-const int minSpeedBall = 6;
-const int maxSpeedBall = 7;
-int initialRadius = 20;
-int ballRadius = 20;
-int powerBallRadius = 15;
-int randBallPosition; 
-Vector2 ballPosition;
-Vector2 ballSpeed;
-Vector2 powerBall;
+const int minSpeedBall = 6; //ball
+const int maxSpeedBall = 7; //ball
+int initialRadius = 20; //ball
+int ballRadius = 20; //ball
+int powerBallRadius = 15; //ball
+int randBallPosition; //ball
+Vector2 ballPosition; //ball
+Vector2 ballSpeed; //ball
+Vector2 powerBall; //ball
 
-Texture2D p1;
-Texture2D p2;
+Texture2D p1; //texture
+Texture2D p2; //texture
 
-const int startPoints = 0;
-const int winPoints = 4;
-int pointsP1;
-int pointsP2;
-int player1Wins;
-int player2Wins;
-int games;
-int gamesToWin = 3;
-int incrementGames = 1;
-int initialGames = 0;
+const int startPoints = 0; //gameplay
+const int winPoints = 4; //gameplay
+int pointsP1; //gameplay
+int pointsP2; //gameplay
+int player1Wins; //gameplay
+int player2Wins; //gameplay
+int games; //gameplay
+int gamesToWin = 3; //gameplay
+int incrementGames = 1; //gameplay
+int initialGames = 0; //gameplay
 
-int sizeColors = 5;
-Color colors[8];
-Color colorBall;
+int sizeColors = 5; //ball
+Color colors[8]; //ball
+Color colorBall; //ball
 
 bool stateGame = true;
 bool stateMenu = true;
 bool stateEndMenu = true;
 bool AI = true;
 
-Image bgImage;
-Texture bgTexture;
-Image imgBall;
-Texture textureBall;
-Image imgPlayer1;
-Texture texturePlayer1;
-Image imgPlayer2;
-Texture texturePlayer2;
+Image bgImage; //image
+Texture bgTexture; //texture
+Image imgBall; //image
+Texture textureBall; //texture
+Image imgPlayer1; //image
+Texture texturePlayer1; //texture
+Image imgPlayer2; //image
+Texture texturePlayer2; //texture
 
-Music bgMusic;
-Sound collisionWave;
+Music bgMusic; //music
+Sound collisionWave; //sound
 
+const int dontCheckCollisionFrames = 10; //collision
 
-const int dontCheckCollisionFrames = 10;
+bool previusFrameCollisionP1 = false; //collision
+int lastCollisionFramesP1 = 0; //collision
+bool previusFrameCollisionP2 = false; //collision
+int lastCollisionFramesP2 = 0; //collision
 
+void InitializeGame(); //initialize
+void Menu(); //menu
+void FinalMenu(); //FinalMenu
+void Game(); //Game
 
-bool previusFrameCollisionP1 = false;
-int lastCollisionFramesP1 = 0;
-bool previusFrameCollisionP2 = false;
-int lastCollisionFramesP2 = 0;
-
-
-
-void InitializeGame();
-void Menu();
-void FinalMenu();
-void Game();
 int main(void)
 {
+	srand(time(NULL));
+	//-----------Initialize--------------
 	InitWindow(screenWidth, screenHeight, "Pong Elias");
 	InitAudioDevice();
 	bgMusic = LoadMusicStream("sounds/background-music.ogg");
 	collisionWave = LoadSound("sounds/dung.wav");
+	//-----------Initialize--------------
 
 
-	while (true)
+	while (true) //Game Loop
 	{
 		while (stateMenu == true)
 		{
 			Menu();
 		}
 		// Main game loop
-		while (stateGame == true)    // Detect window close button or ESC key
+		while (stateGame == true)
 		{
 			Game();
 			if (IsKeyDown(KEY_ESCAPE))
@@ -111,13 +112,9 @@ int main(void)
 			FinalMenu();
 		}
 
-
 		if (IsKeyDown(KEY_ESCAPE))
 			break;
 	}
-
-
-
 
 	UnloadSound(collisionWave);
 	UnloadMusicStream(bgMusic);
@@ -129,7 +126,7 @@ int main(void)
 	return 0;
 }
 
-void Menu()
+void Menu() //Menu
 {
 	if (!IsMusicPlaying(bgMusic))
 	{
@@ -154,8 +151,9 @@ void Menu()
 	}
 	EndDrawing();
 }
-void InitializeGame()
+void InitializeGame() //initialize
 {
+	//------color-------
 	colorBall = WHITE;
 	colors[0] = BLUE;
 	colors[1] = RED;
@@ -165,24 +163,31 @@ void InitializeGame()
 	colors[5] = GOLD;
 	colors[6] = VIOLET;
 	colors[7] = DARKBROWN;
+	//-------color-------
 
+	//-------initBall---------
 	randBallPosition = rand() % 2;
-	
+
 	ballPosition.x = (float)screenWidth / 2;
 	ballPosition.y = (float)screenHeight / 2;
 	ballSpeed.x = 0;
 	ballSpeed.y = 0;
+	//--------initBall-----------
 
+	//--------initPowerUp---------
 	int random1 = 0;
 	random1 = GetRandomValue(randomPowerUPW1, randomPowerUPW2);
 	powerBall.x = random1;
 	int random2 = 0;
 	random2 = GetRandomValue(randomPowerUPH1, randomPowerUPH2);
 	powerBall.y = random2;
+	//--------initPowerUp---------
 
 	// Set our game to run at 60 frames-per-second
 	SetTargetFPS(60);
 	//--------------------------------------------------------------------------------------
+
+	//---------initPlayers-----------
 	player1.x = player1X;
 	player1.y = player1Y;
 	player1.width = 25;
@@ -196,9 +201,10 @@ void InitializeGame()
 	player2.height = 100;
 	player2.color = colors[rand() % 7];
 	p2 = LoadTexture("img/player2.png");
-	 
-	ballSpeed.x = minSpeedBall + rand() % (maxSpeedBall - minSpeedBall);
-	ballSpeed.y = minSpeedBall + rand() % (maxSpeedBall - minSpeedBall);
+	//---------initPlayers-----------
+
+	ballSpeed.x = minSpeedBall + rand() % (maxSpeedBall - minSpeedBall); //ball
+	ballSpeed.y = minSpeedBall + rand() % (maxSpeedBall - minSpeedBall); //ball
 	bgImage = LoadImage("img/bg.png");
 	bgTexture = LoadTextureFromImage(bgImage);
 	UnloadImage(bgImage);
@@ -228,7 +234,7 @@ void Game()
 	DrawTexture(p2, player2.x, player2.y, WHITE);
 	PlayMusicStream(bgMusic);
 	//Conditions
-	switch (randBallPosition)
+	switch (randBallPosition) //ball
 	{
 	case 0:
 		ballPosition.x += ballSpeed.x;
@@ -241,7 +247,7 @@ void Game()
 	}
 	//Player 1
 
-	if (previusFrameCollisionP1)
+	if (previusFrameCollisionP1) //collisions
 	{
 		lastCollisionFramesP1++;
 		if (lastCollisionFramesP1 >= dontCheckCollisionFrames)
@@ -252,7 +258,7 @@ void Game()
 	}
 
 
-	if (previusFrameCollisionP2)
+	if (previusFrameCollisionP2) //collisions
 	{
 		lastCollisionFramesP2++;
 		if (lastCollisionFramesP2 >= dontCheckCollisionFrames)
@@ -262,7 +268,7 @@ void Game()
 		}
 	}
 
-	if (CheckCollisionCircleRec(ballPosition, ballRadius, player1))
+	if (CheckCollisionCircleRec(ballPosition, ballRadius, player1)) //collisions
 	{
 		if (!previusFrameCollisionP1)
 		{
@@ -275,7 +281,7 @@ void Game()
 		
 	}
 	//Player 2
-	if (CheckCollisionCircleRec(ballPosition, ballRadius, player2))
+	if (CheckCollisionCircleRec(ballPosition, ballRadius, player2)) //collisions
 	{
 
 		if (!previusFrameCollisionP2)
@@ -288,7 +294,8 @@ void Game()
 
 		}
 	}
-	if (CheckCollisionCircles(ballPosition, ballRadius, powerBall, powerBallRadius))
+	//----------Collisions------------
+	if (CheckCollisionCircles(ballPosition, ballRadius, powerBall, powerBallRadius)) //collisions
 	{
 		ballRadius = 40;
 		if (ballPosition.x > halfScreenWidth)
@@ -304,6 +311,8 @@ void Game()
 	}
 	if (CheckCollisionCircleRec(ballPosition, ballRadius, player1) == 3 || CheckCollisionCircleRec(ballPosition, ballRadius, player2) == 3)
 		ballRadius = initialRadius;
+	//----------Collisions------------
+	//----------Ball Collisions------------
 	//Walls
 	if ((ballPosition.y >= (screenHeight - ballRadius)) || (ballPosition.y <= ballRadius)) ballSpeed.y *= -1.0f;
 
@@ -330,6 +339,8 @@ void Game()
 		player2.y = screenHeight - player2.height;	
 	if (player2.y < minScreenH)
 		player2.y = 0;
+	//----------Ball Collisions------------
+	//----------Player Points------------
 	if (pointsP1 >= winPoints)
 	{
 		games += incrementGames;
@@ -349,9 +360,10 @@ void Game()
 		stateGame = false;
 		stateEndMenu = true;
 	}
+	//----------Player Points------------
 	EndDrawing();
 }
-void FinalMenu()
+void FinalMenu() //Final Menu
 {
 	BeginDrawing();
 	ClearBackground(BLACK);
